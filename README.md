ea99c28f84ac15a12761d16343f5c27638ec31fd
tiagofribeiro <tiagofr98@gmail.com>
2022-05-18 05:30:53 -0300

    Add task working
    Added ListView with dynamic data

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # flutter_todo_list
 
-A Flutter project 
+A Flutter app with a to-do list, showcasing some intermediary functionalities. 
 
-## Getting Started
+<br>
 
-This project is a starting point for a Flutter application.
+### Widgets and parameters
 
-A few resources to get you started if this is your first Flutter project:
+----
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- TextField
+    - Decoration
+    - Controller
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- ElevatedButton
+    - onPressed()
+
+- SizedBox
+
+- Icon
+
+- Flexible
+
+- ListView
+    - ListTile
+    - shrinkWrap
--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController taskController = TextEditingController();
+
+  List<String> tasks = [];
 
   @override
   Widget build(BuildContext context) {
@@ -9,27 +18,76 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicionar uma tarefa',
-                    hintText: 'Ex. Estudar Flutter',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: taskController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicionar uma tarefa',
+                        hintText: 'Ex. Estudar Flutter',
+                      ),
+                    ),
                   ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      String task = taskController.text;
+                      setState(() {
+                        tasks.add(task);
+                      });
+                      taskController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff00d7f4),
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (String task in tasks)
+                      ListTile(
+                        title: Text(task),
+                        subtitle: Text(DateTime.now().toString()),
+                        onTap: () {
+                          print(task);
+                        },
+                      ),
+                  ],
                 ),
               ),
-              SizedBox(
-                width: 8,
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui 0 tarefas pendentes',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff00d7f4),
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Text('Limpar Tudo'),
+                  )
+                ],
               ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Icon(
-                  Icons.add,
-                  size: 30,
-                ),
-              )
             ],
           ),
         ),
